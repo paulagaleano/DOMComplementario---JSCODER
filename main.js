0b51634d2eb8c2e79e03230b9ea34e6d578f8fc1
Paula <paulagaleano@live.com.ar>
2022-07-05 22:05:43 -0300

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+/* DESAFIO COMPLEMENTARIO DOM */
+
+let saludo = document.getElementById("saludo")
+
+const nombreeUsuario = prompt ("Ingrese su nombre!")
+
+saludo.innerText = "Hola " +nombreeUsuario+"!";
+
